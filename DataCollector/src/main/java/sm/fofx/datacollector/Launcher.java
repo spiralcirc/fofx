c9439562b946337ec0d8acf9322644f4c3ee2277
spiralcirc <spiralcirc@gmail.com>
2015-03-11 00:37:25 -0400

    Quartz scheduler try

--- DataCollector/src/main/java/sm/fofx/datacollector/Launcher.java
+++ DataCollector/src/main/java/sm/fofx/datacollector/Launcher.java
@@ -1,11 +1,23 @@
 package sm.fofx.datacollector;
 
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+
 /**
  * Created  on 1/25/2015.
  */
 public class Launcher {
 
     public static void main(String args[]){
-        
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            // and start it off
+            scheduler.start();
+
+            scheduler.shutdown();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
     }
 }

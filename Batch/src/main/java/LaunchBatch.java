a9212beecf60d69b65507c0569bf0a4ae58ab3de
spiralcirc <spiralcirc@gmail.com>
2015-03-11 22:44:57 -0400

    Spring changes

--- Batch/src/main/java/LaunchBatch.java
+++ Batch/src/main/java/LaunchBatch.java
@@ -1,20 +1,9 @@
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.impl.StdSchedulerFactory;
 
 /**
  * Created on 3/11/2015.
  */
 public class LaunchBatch {
     public static void main(String args[]){
-        try {
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            // and start it off
-            scheduler.start();
 
-            scheduler.shutdown();
-        } catch (SchedulerException e) {
-            e.printStackTrace();
-        }
     }
 }
--- DataCollector/src/main/java/sm/fofx/datacollector/Launcher.java
+++ DataCollector/src/main/java/sm/fofx/datacollector/Launcher.java
@@ -1,10 +1,18 @@
 package sm.fofx.datacollector;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created  on 1/25/2015.
  */
 public class Launcher {
 
     public static void main(String args[]) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("Application.xml");
+
+       Object o =  context.getBean("mybean");
+
     }
 }
